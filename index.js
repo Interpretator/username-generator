cc87957f74e51950bc13e194daca4aa9e4591640
naz <nazar3223@gmail.com>
2021-01-23 22:38:40 +0200

    add option to capitalize first letter

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-'use strict';
+const utils = require('./utils');
 
 const data = {
-    names: require('./names.json'),
-    adjectives: require('./adjectives.json')
-}
-let seperator = '-'
+  names: require('./names.json'),
+  adjectives: require('./adjectives.json'),
+};
+
+let seperator = ' ';
 
 function setNames(names) {
-    data.names = names
-};
+  data.names = names;
+}
 
 function setAdjectives(adjectives) {
-    data.adjectives = adjectives
-};
+  data.adjectives = adjectives;
+}
 
 function setSeperator(new_seperator) {
-    seperator = new_seperator
-};
+  seperator = new_seperator;
+}
 
-function generate() {
-    const ran_a = Math.floor(Math.random() * data.names.length)
-    const ran_b = Math.floor(Math.random() * data.adjectives.length)
-    const ran_suffix = Math.floor(Math.random() * 100)
-    return `${data.adjectives[ran_b]}${seperator}${data.names[ran_a]}${ran_suffix}`
-};
+function generate({ isFirstLetterUpperCase = false } = {}) {
+  const ran_a = Math.floor(Math.random() * data.names.length);
+  const ran_b = Math.floor(Math.random() * data.adjectives.length);
+
+  let first = data.adjectives[ran_b];
+  let second = data.names[ran_a];
+
+  if (isFirstLetterUpperCase) {
+    first = utils.capitalizeFirstLetter(first);
+    second = utils.capitalizeFirstLetter(second);
+  }
+
+  return `${first}${seperator}${second}`;
+}
 
 module.exports = {
-    setNames: setNames,
-    setAdjectives: setAdjectives,
-    setSeperator: setSeperator,
-    generate: generate
-}
+  setNames: setNames,
+  setAdjectives: setAdjectives,
+  setSeperator: setSeperator,
+  generate: generate,
+};
